5661952b7b523db43daeb6eda584a1419b7c74ab
Tim Meyerdiercks <timmd.development.contact@gmail.com>
2021-07-11 20:44:05 +0200

    Create the tableView Outlet in the MVC and create the extensions for it.

--- Vertragsmanager/ViewController.swift
+++ Vertragsmanager/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var contractTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
 }
 
