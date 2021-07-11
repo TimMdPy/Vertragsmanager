7edabc41ffd22f1aaa440c3aff0bff5ee9de12f8
Tim Meyerdiercks <timmd.development.contact@gmail.com>
2021-07-11 20:33:56 +0200

    Create the new ItemTableViewCell Subclass.

---
+++ Vertragsmanager/ItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.swift
+//  Vertragsmanager
+//
+//  Created by Tim Meyerdiercks on 11.07.21.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
