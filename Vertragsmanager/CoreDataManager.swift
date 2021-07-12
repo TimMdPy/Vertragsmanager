d424a3fd3360501737c22cb6c2e4c4b9f513db91
Tim Meyerdiercks <timmd.development.contact@gmail.com>
2021-07-12 15:09:48 +0200

    Create the Core Data entiti and create the CoreDataManager file.

---
+++ Vertragsmanager/CoreDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataManager.swift
+//  Vertragsmanager
+//
+//  Created by Tim Meyerdiercks on 12.07.21.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    // Pro Sektion / Kategorie ein Array
+    var carContractCategory = [Vertrag]()
+    var phoneContractCategory = [Vertrag]()
+    var homeContractCategory = [Vertrag]()
+    var insuranceContractCategory = [Vertrag]()
+}
+
